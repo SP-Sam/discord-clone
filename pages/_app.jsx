5089907427463c48a7400129c5ac9f20b849515c
Samuel Pereira <gmr.quest.10@gmail.com>
2022-03-26 16:19:59 -0300

    refactor: Muda titulo e icone de pagina

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,9 +1,14 @@
+import Head from 'next/head';
 import Provider from '../context/Provider';
 import GlobalStyle from '../styles/GlobalStyle';
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider>
+      <Head>
+        <title>SP-Sam | Discord clone</title>
+        <link rel="shortcut icon" href="https://img.icons8.com/external-tal-revivo-color-tal-revivo/24/000000/external-discord-chat-for-social-gaming-between-peers-logo-color-tal-revivo.png" type="image/x-icon" />
+      </Head>
       <GlobalStyle />
       <Component {...pageProps} />
     </Provider>
